48ae27bf400f3d3097b182ec9c9af0d8fed4f840
thachphongphong@gmail.com <thachphongphong@gmail.com>
2016-09-17 14:14:25 +0700

    change logo

--- resources/views/promotion_section.blade.php
+++ resources/views/promotion_section.blade.php
@@ -5,7 +5,6 @@
             @foreach ($promotions as $pro)
                 <h2 class="mg-sec-left-title">{{$pro->description}} : {{$pro->sale_off}}</h2>
                 <p>{{$constants['room']['promotion_app']}} {{$constants['room']['promotion_from']}} :  {{ date('d-m-Y', strtotime($pro->apply_time_from)) }} {{$constants['room']['promotion_to']}} :  {{ date('d-m-Y', strtotime($pro->apply_time_to)) }}</p>
-                 <hr/>
             @endforeach
         </div>
     </div>
